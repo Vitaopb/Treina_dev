37ea76117cc91aa58b47e8df1d9989fc1e28fea8
Vitaopb <victor.lieuthier@gmail.com>
2022-01-14 17:19:45 -0300

    aula 04 - While/Loops

--- logica_ruby/cookbook.rb
+++ logica_ruby/cookbook.rb
@@ -2,9 +2,9 @@ puts "Bem vindo ao Cookbook do Ruby"
 
 receitas = []
 
-add = "s"
+adicionar = "s"
 
-while add == "s" do
+while adicionar == "s" do
   puts "Digite o nome da receita: "
   name = gets.chomp
   receitas.push(name)
@@ -12,7 +12,7 @@ while add == "s" do
   puts "Receita #{name} cadastrada com sucesso!"
 
   puts "Quer adicionar uma nova receita? (s/n)"
-  add = gets.chomp
+  adicionar = gets.chomp
 end
 
 puts "========== Receitas cadastradas =========="
