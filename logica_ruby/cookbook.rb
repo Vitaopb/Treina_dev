2547b0983eba880d13b727bb635e0478795bc833
Vitaopb <victor.lieuthier@gmail.com>
2022-01-14 16:21:19 -0300

    aula 03 - array

--- logica_ruby/cookbook.rb
+++ logica_ruby/cookbook.rb
@@ -1,6 +1,12 @@
 puts "Bem vindo ao Cookbook do Ruby"
 puts "Digite o nome da receita:"
 
+receitas = []
+
 name = gets.chomp
 
+receitas.push(name)
+
 puts "Receita #{name} cadastrada com sucesso!"
+
+puts receitas
