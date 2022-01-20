5b5f874b64280cb5604c760b24089833adf74f9e
Vitaopb <victor.lieuthier@gmail.com>
2022-01-20 00:02:39 -0300

    Aula 05, 06 e 07 - condicionais, hash e métodos.

--- logica_ruby/cookbook.rb
+++ logica_ruby/cookbook.rb
@@ -1,21 +1,58 @@
-puts "Bem vindo ao Cookbook do Ruby"
-
-receitas = []
-
-adicionar = "s"
-
-while adicionar == "s" do
-  puts "Digite o nome da receita: "
-  name = gets.chomp
-  receitas.push(name)
-  
-  puts "Receita #{name} cadastrada com sucesso!"
-
-  puts "Quer adicionar uma nova receita? (s/n)"
-  adicionar = gets.chomp
+def welcome()
+  puts "Bem vindo ao Cookbook do Ruby"
 end
 
-puts "========== Receitas cadastradas =========="
-receitas.each do |receita|
-  puts receita
+def menu_escolha()
+  puts "[1] Adicionar receita"
+  puts "[2] Ver todas as receitas"
+  puts "[3] Sair"
+  
+  print "Escolha uma opção: "
+
+  return gets.chomp
+end
+
+
+def add_receita()
+  puts "Digite o nome da receita: "
+  nome_receita = gets.chomp
+  puts "Digite a descrição da receita: "
+  descricao_receita = gets.chomp
+  
+  puts
+  puts "Receita adicionada com sucesso!"
+  puts
+  return {
+      "nome": nome_receita,
+      "descrição": descricao_receita
+      }
+end
+
+def listar_receitas(receitas)
+  puts "Receitas cadastradas:"
+  puts
+    receitas.each do |receita|
+      puts "* #{receita[:nome]}: #{receita[:descrição]}"
+    end
+end
+
+
+receitas = []
+welcome()
+
+escolha = menu_escolha()
+
+while escolha != "3" do
+  case escolha
+  when "1"
+    receitas.push(add_receita())
+  when "2"
+    listar_receitas(receitas)
+  else
+    puts "Opção inválida"
+  end
+
+  puts
+
+  escolha = menu_escolha()
 end
